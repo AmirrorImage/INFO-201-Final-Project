f96f6a2e1cb8af999489c417445c84860e046967
Leary Labanlic <learylabanlic@Learys-MacBook-Pro.local>
2021-02-17 19:32:26 -0800

    Loaded libraries and data. Began analysis for race pie chart.

--- Exploratory-Analysis/Chart-3-Leary.R
+++ Exploratory-Analysis/Chart-3-Leary.R
@@ -1,1 +1,31 @@
 # Chart 3
+
+
+# Load Libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+
+data <- read.csv("Use_Of_Force.csv")
+View(data)
+
+
+# Chart idea : pie chart that sees proportion of each affected race to the whole
+# Column needed : Subject_Race
+
+race <- data %>% 
+  select(Subject_Race) %>% 
+  View()
+
+all_races <- unique(data$Subject_Race)
+View(all_races)
+
+
+  
+pie(race, )
+  
+  
+
